98f0526f8ce197e8b6238e6d84483fb4485b3ec1
eugeneLuck <eugeneLuck@gmail.com>
2024-01-17 20:29:57 +0200

    CommunityToolkitMVVM

--- MyMediaCollection/MyMediaCollection/ViewModels/MainViewModel.cs
+++ MyMediaCollection/MyMediaCollection/ViewModels/MainViewModel.cs
@@ -6,84 +6,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 using MyMediaCollection.Enums;
 using MyMediaCollection.Model;
 
 namespace MyMediaCollection.ViewModels
 {
-    public class MainViewModel : BindableBase
+    public partial class MainViewModel : ObservableObject
     {
-        IList<string> _mediums;
+        [ObservableProperty]
         string _selectedMedium;
-        
+        [ObservableProperty]
         ObservableCollection<MediaItem> _items;
-        ObservableCollection<MediaItem> _allItems;
+        [ObservableProperty]
+        IList<string> _mediums;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(DeleteCommand))]
         MediaItem _selectedItem;
 
-        public ICommand DeleteCommand { get; set; }
-        public ICommand AddEditCommand { get; set; }
+        //public ICommand DeleteCommand { get; set; }
+        //public ICommand AddEditCommand { get; set; }
 
+        ObservableCollection<MediaItem> _allItems;
         int _additionaItemCount = 1;
 
         public MainViewModel()
         {
             PopulateData();
-            DeleteCommand = new RelayCommand(DeleteItem, CanDeleteItem);
-            AddEditCommand = new RelayCommand(AddOrEditItem);
-        }
-
-        public IList<string> Mediums
-        {
-            get => _mediums;
-            set => SetProperty(ref _mediums, value);
-        }
-
-        public string SelectedMedium
-        {
-            get => _selectedMedium;
-            set
-            {
-                SetProperty(ref _selectedMedium, value);
-
-                //Items.Clear();
-                //foreach(var item in _allItems)
-                //{
-                //    if(string.IsNullOrWhiteSpace(_selectedMedium) ||
-                //        _selectedMedium == "All" ||
-                //        _selectedMedium == item.MediaType.ToString())
-                //    {
-                //        Items.Add(item);
-                //    }
-                //}
-                var selected = _allItems.Where(x => 
-                    _selectedMedium is null ||
-                    string.IsNullOrWhiteSpace(_selectedMedium) ||
-                    _selectedMedium == "All" ||
-                    _selectedMedium.ToString() == x.MediaType.ToString()
-                    );
-                Items = new ObservableCollection<MediaItem>(selected);
-            }
-        }
-
-        public ObservableCollection<MediaItem> Items
-        { 
-            get => _items; 
-            set => SetProperty(ref _items, value);  
-        }
-
-        public MediaItem SelectedItem
-        {
-            get => _selectedItem;
-            set
-            {
-                SetProperty(ref _selectedItem, value);
-                ((RelayCommand)DeleteCommand).RaiseCanExecuteChanged();
-            }
+            //DeleteCommand = new RelayCommand(DeleteItem, CanDeleteItem);
+            //AddEditCommand = new RelayCommand(AddOrEditItem);
         }
 
         public void PopulateData()
         {
-            _mediums = new List<string>
+            //_mediums = new List<string>
+            Mediums = new List<string>
             {
                 "All",
                 nameof(ItemType.Book),
@@ -138,17 +97,32 @@ namespace MyMediaCollection.ViewModels
                 bluRay
             };
 
-            _items = new ObservableCollection<MediaItem>(_allItems);
+            //_items = new ObservableCollection<MediaItem>(_allItems);
+            Items = new ObservableCollection<MediaItem>(_allItems);
         }
 
-        void DeleteItem()
+
+        partial void OnSelectedMediumChanged(string value)
+        {
+            var selected = _allItems.Where(x =>
+                _selectedMedium is null ||
+                string.IsNullOrWhiteSpace(_selectedMedium) ||
+                _selectedMedium == "All" ||
+                _selectedMedium.ToString() == x.MediaType.ToString()
+                );
+            Items = new ObservableCollection<MediaItem>(selected);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanDeleteItem))]
+        void Delete()
         {
             _allItems.Remove(SelectedItem);
             Items.Remove(SelectedItem);
         }
         bool CanDeleteItem() => _selectedItem != null;
 
-        void AddOrEditItem()
+        [RelayCommand]
+        void AddEdit()
         {
             const int startingItemCount = 3;
             var newItem = new MediaItem
@@ -168,5 +142,54 @@ namespace MyMediaCollection.ViewModels
             Items.Add(newItem);
             _additionaItemCount++;
         }
+        //public IList<string> Mediums
+        //{
+        //    get => _mediums;
+        //    set => SetProperty(ref _mediums, value);
+        //}
+/*
+        public string SelectedMedium
+        {
+            get => _selectedMedium;
+            set
+            {
+                SetProperty(ref _selectedMedium, value);
+
+                //Items.Clear();
+                //foreach(var item in _allItems)
+                //{
+                //    if(string.IsNullOrWhiteSpace(_selectedMedium) ||
+                //        _selectedMedium == "All" ||
+                //        _selectedMedium == item.MediaType.ToString())
+                //    {
+                //        Items.Add(item);
+                //    }
+                //}
+                var selected = _allItems.Where(x => 
+                    _selectedMedium is null ||
+                    string.IsNullOrWhiteSpace(_selectedMedium) ||
+                    _selectedMedium == "All" ||
+                    _selectedMedium.ToString() == x.MediaType.ToString()
+                    );
+                Items = new ObservableCollection<MediaItem>(selected);
+            }
+        }
+
+        //public ObservableCollection<MediaItem> Items
+        //{ 
+        //    get => _items; 
+        //    set => SetProperty(ref _items, value);  
+        //}
+
+        public MediaItem SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                ((RelayCommand)DeleteCommand).RaiseCanExecuteChanged();
+            }
+        }
+*/
     }
 }
